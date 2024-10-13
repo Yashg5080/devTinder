ca1bcab3919fd58d20faf7921ef8d04ea93173b1
Yash Gupta <yashgupta50357@gmail.com>
2024-10-13 16:24:18 +0530

    Exploring Routes and Regex

--- src/app.js
+++ src/app.js
@@ -11,6 +11,24 @@ app.get("/products", (req, res) => {
   res.send("Products");
 });
 
+// Post Request
+app.post("/products", (req, res) => {
+  res.send("Products");
+});
+
+// Dynamic Route with query parameter
+app.post("/products/:id", (req, res) => {
+  console.log(req.query, req.params);
+  res.send("Products" + req.params.id );
+});
+
+
+// Dynamic Route with Regular Expression and we can also write regular expression
+// in the form of string
+app.post(/.*fly$/, (req, res) => {
+  res.send("Reg");
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
