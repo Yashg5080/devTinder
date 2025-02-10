9f5b3bbbf33177271bae82bba3d46921de84a004
Yash Gupta <yashgupta50357@gmail.com>
2025-02-10 22:35:17 +0530

    Adding validations to apis

--- src/app.js
+++ src/app.js
@@ -7,13 +7,13 @@ const User = require('./models/user');
 // This middleware will help us parse the request body of incoming requests
 app.use(express.json());
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
   const user = new User(req.body);
   try {
-    user.save();
+    await user.save();
     res.send('sign up done');
   } catch (err) {
-    res.send(err);
+    res.status(400).send(err.message);
   }
 });
 
@@ -48,15 +48,39 @@ app.delete('/user', async (req, res) => {
   }
 });
 
-app.patch('/user', async (req, res) => {
+app.patch('/user/:userId', async (req, res) => {
   try {
+    const userId = req.params?.userId
+
+    // These are the custom validations for the fields that can be updated
+    ALLOWED_UPDATES = new Set(['photoUrl', 'about', 'gender', 'skills']);
+    const isUpdateAllowed = Object.keys(req.body).every((key) => {
+      return ALLOWED_UPDATES.has(key);
+    });
+
+    if (!isUpdateAllowed) {
+      return res.status(400).send('Invalid field for update');
+    }
+
+    if (req.body.skills) {
+      if (!Array.isArray(req.body.skills)) {
+        return res.status(400).send('Skills should be an array');
+      } 
+      else if (req.body.skills.some((skill) => typeof skill !== 'string')) {
+        return res.status(400).send('Skills should be an array of strings');
+      } 
+      else if (req.body.skills.length > 5) {
+         throw res.status(400).send('Skills should not be more than 5');
+      }
+    }
+
     const user = await User.findByIdAndUpdate(
-      {_id: req.body.userId},
+      {_id: userId},
       req.body,
-      {returnDocument: 'after'}
+      {returnDocument: 'after', runValidators: true}
     );
     if (!user) {
-      res.status(404).send('No user found');
+      return res.status(404).send('No user found');
     }
     res.send(user);
   } catch (err) {
--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,75 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const schema = mongoose.Schema({
     firstName: {
         type: String,
-        required: true
+        required: true,
+        minLength: 2,
+        maxLength: 100
     },
     lastName: {
         type: String,
-        required: true
+        required: true,
+        minLength: 2,
+        maxLength: 100
+    },
+    password: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {    
+                throw new Error("Weak Password");
+            }
+            return value;
+        }
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: function(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Invalid Email");
+                }
+            },
+            message: "Invalid Email"
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
+    },
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female", "other"].includes(value)) {
+                throw new Error("Invalid Gender")
+            }
+            return value;
+        },
+    },
+    photoUrl: {
+        type: String,
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid URL")
+            }
+            return value;
+        }
+    },
+    about: {
+        type: String,
+        default: "This is a default about"
+    },
+    skills: {
+        type: [String]
     }
+}, {
+    timestamps: true
 }); 
 
 module.exports = mongoose.model('User', schema);
